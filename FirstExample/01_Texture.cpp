88f374b61f944c3a5102a0cd28a968529268a5ec
shilaranidas <shilarani.das@georgebrown.ca>
2020-12-21 01:09:03 -0500

    Add project files.

---
+++ FirstExample/01_Texture.cpp
@@ -0,0 +1,400 @@
+﻿
+///////////////////////////////////////////////////////////////////////
+//
+// 01_Texture.cpp
+//
+///////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+#include <cstdlib>
+#include <ctime>
+#include "vgl.h"
+#include "LoadShaders.h"
+#include "glm\glm.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define FPS 60
+#define MOVESPEED 0.1f
+#define TURNSPEED 0.05f
+#define X_AXIS glm::vec3(1,0,0)
+#define Y_AXIS glm::vec3(0,1,0)
+#define Z_AXIS glm::vec3(0,0,1)
+#define XY_AXIS glm::vec3(1,1,0)
+#define YZ_AXIS glm::vec3(0,1,1)
+#define XZ_AXIS glm::vec3(1,0,1)
+
+
+enum keyMasks {
+	KEY_FORWARD =  0b00000001,		// 0x01 or 1 or 01
+	KEY_BACKWARD = 0b00000010 ,		// 0x02 or 2 or 02
+	KEY_LEFT = 0b00000100,		
+	KEY_RIGHT = 0b00001000,
+	KEY_UP = 0b00010000,
+	KEY_DOWN = 0b00100000,
+	KEY_MOUSECLICKED = 0b01000000
+	// Any other keys you want to add.
+};
+
+// IDs.
+GLuint vao, ibo, points_vbo, colours_vbo, mvp_ID;
+
+// Matrices.
+glm::mat4 MVP, View, Projection;
+
+// Our bitflags. 1 byte for up to 8 keys.
+unsigned char keys = 0; // Initialized to 0 or 0b00000000.
+
+// Camera and transform variables.
+float scale = 0.5f, angle = 0.0f;
+glm::vec3 position, frontVec, worldUp, upVec, rightVec; // Set by function
+GLfloat pitch, yaw;
+int lastX, lastY;
+
+// Texture variables.
+GLuint textureID;
+GLint width, height, bitDepth;
+
+// Geometry data.
+GLshort cube_indices[] = {
+	// Front.
+	0, 1, 2,
+	2, 3, 0
+};
+
+GLfloat cube_vertices[] = {
+	//	x,		y,		z,		u,		v
+		-0.9f, -0.9f, 0.9f,		0.0f, 0.0f,		// 0.
+		0.9f, -0.9f, 0.9f,		1.0f, 0.0f,		// 1.
+		0.9f, 0.9f, 0.9f,		1.0f, 1.0f,		// 2.
+		-0.9f, 0.9f, 0.9f,		0.0f, 1.0f		// 3.
+};
+
+GLfloat colours[] = {
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f
+};
+
+void timer(int);
+
+void resetView()
+{
+	position = glm::vec3(0.0f, 0.0f, 3.0f);
+	frontVec = glm::vec3(0.0f, 0.0f, -1.0f);
+	worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+	pitch = 0.0f;
+	yaw = -90.0f;
+	// View will now get set only in transformObject
+}
+
+void init(void)
+{
+	srand((unsigned)time(NULL));
+	//Specifying the name of vertex and fragment shaders.
+	ShaderInfo shaders[] = {
+		{ GL_VERTEX_SHADER, "triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+
+	//Loading and compiling shaders
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);	//My Pipeline is set up
+
+	mvp_ID = glGetUniformLocation(program, "MVP");
+
+	// Projection matrix : 45∞ Field of View, aspect ratio, display range : 0.1 unit <-> 100 units
+	Projection = glm::perspective(glm::radians(45.0f), 1.0f / 1.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	// Projection = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 100.0f); // In world coordinates
+
+	// Camera matrix
+	resetView();
+
+	// Image loading.
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned char* image = stbi_load("alex.jpg", &width, &height, &bitDepth, 0);
+	if (!image) {
+		cout << "Unable to load file!" << endl;
+		// Could add a return too if you modify init.
+	}
+
+	glGenTextures(1, &textureID);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glUniform1i(glGetUniformLocation(program, "texture0"), 0);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	// Clean up. But we don't want to unbind the texture or we cannot use it.
+	stbi_image_free(image);
+
+	vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+		glGenBuffers(1, &ibo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
+
+		points_vbo = 0;
+		glGenBuffers(1, &points_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(cube_vertices[0]) * 5, 0);
+		glEnableVertexAttribArray(0);
+
+		// Now for the UV/ST values.
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(cube_vertices[0]) * 5, (void*)(sizeof(cube_vertices[0]) * 3));
+		glEnableVertexAttribArray(2);
+
+		colours_vbo = 0;
+		glGenBuffers(1, &colours_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, colours_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(1);
+		//glBindBuffer(GL_ARRAY_BUFFER, 0); // Can optionally unbind the buffer to avoid modification.
+
+	glBindVertexArray(0); // Can optionally unbind the vertex array to avoid modification.
+
+	// Enable depth test.
+	glEnable(GL_DEPTH_TEST);
+	//glEnable(GL_CULL_FACE);
+	//glFrontFace(GL_CCW);
+	//glCullFace(GL_BACK);
+
+	timer(0); 
+}
+
+//---------------------------------------------------------------------
+//
+// calculateView
+//
+void calculateView()
+{
+	frontVec.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	frontVec.y = sin(glm::radians(pitch));
+	frontVec.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	frontVec = glm::normalize(frontVec);
+	rightVec = glm::normalize(glm::cross(frontVec, worldUp));
+	upVec = glm::normalize(glm::cross(rightVec, frontVec));
+
+	View = glm::lookAt(
+		position, // Camera position
+		position + frontVec, // Look target
+		upVec); // Up vector
+}
+
+//---------------------------------------------------------------------
+//
+// transformModel
+//
+void transformObject(float scale, glm::vec3 rotationAxis, float rotationAngle, glm::vec3 translation) {
+	glm::mat4 Model;
+	Model = glm::mat4(1.0f);
+	Model = glm::translate(Model, translation);
+	Model = glm::rotate(Model, glm::radians(rotationAngle), rotationAxis);
+	Model = glm::scale(Model, glm::vec3(scale));
+	
+	calculateView();
+	
+	MVP = Projection * View * Model;
+	glUniformMatrix4fv(mvp_ID, 1, GL_FALSE, &MVP[0][0]);
+}
+
+//---------------------------------------------------------------------
+//
+// display
+//
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBindVertexArray(vao);
+	transformObject(scale, YZ_AXIS, angle/*+=1.0f*/, glm::vec3(0.0f, 0.0f, 0.0f));
+
+	//Ordering the GPU to start the pipeline
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+	glBindVertexArray(0); // Can optionally unbind the vertex array to avoid modification.
+
+	glutSwapBuffers(); // Now for a potentially smoother render.
+}
+
+void parseKeys()
+{
+	if (keys & KEY_FORWARD)
+		position += frontVec * MOVESPEED;
+	else if (keys & KEY_BACKWARD)
+		position -= frontVec * MOVESPEED;
+	if (keys & KEY_LEFT)
+		position -= rightVec * MOVESPEED;
+	else if (keys & KEY_RIGHT)
+		position += rightVec * MOVESPEED;
+	if (keys & KEY_UP)
+		position.y += MOVESPEED;
+	else if (keys & KEY_DOWN)
+		position.y -= MOVESPEED;
+}
+
+void timer(int) { // essentially our update()
+	parseKeys();
+	glutPostRedisplay();
+	glutTimerFunc(1000/FPS, timer, 0); // 60 FPS or 16.67ms.
+}
+
+//---------------------------------------------------------------------
+//
+// keyDown
+//
+void keyDown(unsigned char key, int x, int y) // x and y is mouse location upon key press.
+{
+	switch (key)
+	{
+	case 'w':
+		if (!(keys & KEY_FORWARD))
+			keys |= KEY_FORWARD; break;
+	case 's':
+		if (!(keys & KEY_BACKWARD))
+			keys |= KEY_BACKWARD; break;
+	case 'a':
+		if (!(keys & KEY_LEFT))
+			keys |= KEY_LEFT; break;
+	case 'd':
+		if (!(keys & KEY_RIGHT))
+			keys |= KEY_RIGHT; break;
+	case 'r':
+		if (!(keys & KEY_UP))
+			keys |= KEY_UP; break;
+	case 'f':
+		if (!(keys & KEY_DOWN))
+			keys |= KEY_DOWN; break;
+	}
+}
+
+void keyDownSpec(int key, int x, int y) // x and y is mouse location upon key press.
+{
+	if (key == GLUT_KEY_UP)
+	{
+		if (!(keys & KEY_FORWARD))
+			keys |= KEY_FORWARD;
+	}
+	else if (key == GLUT_KEY_DOWN)
+	{
+		if (!(keys & KEY_BACKWARD))
+			keys |= KEY_BACKWARD;
+	}
+}
+
+void keyUp(unsigned char key, int x, int y) // x and y is mouse location upon key press.
+{
+	switch (key)
+	{
+	case 'w':
+		keys &= ~KEY_FORWARD; break;
+	case 's':
+		keys &= ~KEY_BACKWARD; break;
+	case 'a':
+		keys &= ~KEY_LEFT; break;
+	case 'd':
+		keys &= ~KEY_RIGHT; break;
+	case 'r':
+		keys &= ~KEY_UP; break;
+	case 'f':
+		keys &= ~KEY_DOWN; break;
+	case ' ':
+		resetView();
+	}
+}
+
+void keyUpSpec(int key, int x, int y) // x and y is mouse location upon key press.
+{
+	if (key == GLUT_KEY_UP)
+	{
+		keys &= ~KEY_FORWARD;
+	}
+	else if (key == GLUT_KEY_DOWN)
+	{
+		keys &= ~KEY_BACKWARD;
+	}
+}
+
+void mouseMove(int x, int y)
+{
+	if (keys & KEY_MOUSECLICKED)
+	{
+		pitch += (GLfloat)((y - lastY) * TURNSPEED);
+		yaw -= (GLfloat)((x - lastX) * TURNSPEED);
+		lastY = y;
+		lastX = x;
+	}
+}
+
+void mouseClick(int btn, int state, int x, int y)
+{
+	if (state == 0)
+	{
+		lastX = x;
+		lastY = y;
+		keys |= KEY_MOUSECLICKED; // Flip flag to true
+		glutSetCursor(GLUT_CURSOR_NONE);
+		//cout << "Mouse clicked." << endl;
+	}
+	else
+	{
+		keys &= ~KEY_MOUSECLICKED; // Reset flag to false
+		glutSetCursor(GLUT_CURSOR_INHERIT);
+		//cout << "Mouse released." << endl;
+	}
+}
+
+void clean()
+{
+	cout << "Cleaning up!" << endl;
+	glDeleteTextures(1, &textureID);
+}
+
+//---------------------------------------------------------------------
+//
+// main
+//
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GLUT_MULTISAMPLE);
+	glutSetOption(GLUT_MULTISAMPLE, 8);
+	glutInitWindowSize(1024, 1024);
+	glutCreateWindow("GAME2012 - Week 7");
+
+	glewInit();	//Initializes the glew and prepares the drawing pipeline.
+	init();
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyDown);
+	glutSpecialFunc(keyDownSpec);
+	glutKeyboardUpFunc(keyUp); // New function for third example.
+	glutSpecialUpFunc(keyUpSpec);
+
+	glutMouseFunc(mouseClick);
+	glutMotionFunc(mouseMove); // Requires click to register.
+	
+	atexit(clean); // This GLUT function calls specified function before terminating program. Useful!
+
+	glutMainLoop();
+
+	return 0;
+}
